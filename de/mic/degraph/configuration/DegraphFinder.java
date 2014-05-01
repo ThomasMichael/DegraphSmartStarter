80fcf843938976c3938654e4469c07b0ffb7892b
Thomas Michael <thomas.michael@anmi24.de>
2014-05-01 20:53:29 +0200

    added Test for Filesearch

--- de/mic/degraph/configuration/DegraphFinder.java
+++ de/mic/degraph/configuration/DegraphFinder.java
@@ -2,6 +2,10 @@ package de.mic.degraph.configuration;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * This class search for degraph in filesystem.
@@ -12,6 +16,7 @@ import java.io.FileFilter;
 public class DegraphFinder {
 
 	private final String degraphName = "Degraph.*.jar";
+	private boolean reverse;
 
 	public File find(File root) {
 
@@ -36,12 +41,32 @@ public class DegraphFinder {
 					// found
 					return expected[0];
 				} else {
-					for (File f : current.listFiles()) {
-						search(f, filter);
+					for (File f : iterateOver(current)) {
+						File found = search(f, filter);
+						if (found != null) {
+							return found;
+						}
 					}
 				}
 			}
 		}
 		return null; // nothing found
+	}
+
+	/**
+	 * Seaching Degraph from behind.
+	 * 
+	 * @param reverse
+	 */
+	public void setReverse(boolean reverse) {
+		this.reverse = reverse;
+	}
+
+	private Collection<File> iterateOver(File current) {
+		List<File> fileList = Arrays.asList(current.listFiles());
+		if (reverse) {
+			Collections.reverse(fileList);
+		}
+		return fileList;
 	}
 }
--- de/mic/degraph/configuration/DegraphFinderTest.java
+++ de/mic/degraph/configuration/DegraphFinderTest.java
@@ -8,11 +8,85 @@ import org.junit.Test;
 
 public class DegraphFinderTest {
 
-	// public void
 	@Test
-	public void shouldFindDegraph() throws Exception {
+	public void performanceFinder() {
+		long start = System.currentTimeMillis();
 		DegraphFinder degraphFinder = new DegraphFinder();
-		File find = degraphFinder.find(new File(File.separator + "Temp"));
+		File find = degraphFinder.find(new File(File.separator));
 		assertNotNull(find);
+		long stop = System.currentTimeMillis() - start;
+		System.out.println("Dauer normal:" + stop / 1000);
+	}
+
+	@Test
+	public void performanceFinderReverse() {
+		long start = System.currentTimeMillis();
+		DegraphFinder degraphFinder = new DegraphFinder();
+		degraphFinder.setReverse(true);
+		File find = degraphFinder.find(new File(File.separator));
+		assertNotNull(find);
+		long stop = System.currentTimeMillis() - start;
+		System.out.println("Dauer reverse:" + stop / 1000);
+	}
+
+	@Test
+	public void performanceFinderThread() throws InterruptedException {
+		long start = System.currentTimeMillis();
+
+		final FileWrapper found = new FileWrapper();
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				DegraphFinder degraphFinder = new DegraphFinder();
+				File find = degraphFinder.find(new File(File.separator));
+				found.setFile(find);
+			}
+		};
+		Thread t1 = new Thread(runnable);
+		t1.start();
+
+		Runnable reverse = new Runnable() {
+
+			@Override
+			public void run() {
+				DegraphFinder degraphFinder = new DegraphFinder();
+				degraphFinder.setReverse(true);
+				File find = degraphFinder.find(new File(File.separator));
+				found.setFile(find);
+			}
+		};
+		Thread t2 = new Thread(reverse);
+		t2.start();
+
+		while (true) {
+			if (found.getFile() == null) {
+				Thread.sleep(500);
+			} else {
+				if (t2.isAlive()) {
+					t2.interrupt();
+				}
+				if (t1.isAlive()) {
+					t1.interrupt();
+				}
+				break;
+			}
+		}
+		assertNotNull(found.getFile());
+
+		long stop = System.currentTimeMillis() - start;
+		System.out.println("Dauer Thread:" + stop / 1000);
+	}
+
+	class FileWrapper {
+		File file;
+
+		void setFile(File aFile) {
+			file = aFile;
+		}
+
+		File getFile() {
+			return file;
+		}
 	}
 }
